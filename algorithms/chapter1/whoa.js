2e07fa04b36931a10cee0556f599c5540d01ad27
Candra Davis <candrasd1029@gmail.com>
2016-11-13 22:33:23 -0600

    ending week 1

--- algorithms/chapter1/whoa.js
+++ algorithms/chapter1/whoa.js
@@ -1,5 +1,12 @@
-for (var i = -300000; i <= 300,000; i++) {
-  if (i % 2 != 0) {
-  
+var xOdd = [];
+var sumInt = xOdd[xOdd.length - 1 + xOdd.length - 2];
+
+  for (var i = -300000; i <= 300,000; i++) {
+    if (i % 2 != 0) {
+      xOdd.push(i);
+    }
+    else {
+      continue;
+    }
   }
-}
+console.log(xOdd);
---
+++ webFundamentals/jqueryWork/jqueryFunctions.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  $('button').click(function() {
+    var addTop = $('div.functions.add-class').add('p').css("background", "coral");
+
+    return addTop;
+  });
+});
