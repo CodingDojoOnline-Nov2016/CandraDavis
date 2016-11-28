ad0553a6efb71c8391fca6bf4abf728bbd325399
Candra Davis <candrasd1029@gmail.com>
2016-11-28 12:50:31 -0600

    final addingUser changes Week 3 Assignment

--- webFundamentals/jqueryWork/addingUser/addingUser.js
+++ webFundamentals/jqueryWork/addingUser/addingUser.js
@@ -1,8 +1,15 @@
-var newTableRow = $('<tr><td></td></tr>');
+$(document).ready(function() {
+  console.log('I am ready');
 
-$(document)ready(function() {
-  $('.query-form').on('click', '#addUser', function() {
-    $('table').html(newTableRow);
+  $('.query-form').on('click', '#addUser' , function() {
+    var newTableRow = '<tr><td>'+ $('input.first-name').val() +'</td><td>'+ $('input.last-name').val() +'</td><td>'+ $('input.email').val() +'</td><td>'+ $('input.phone').val() +'</td></tr>';
+      $('tbody').append(newTableRow);
   })
 })
-// {/* <td>input.first-name</td><td>input.last-name</td><td>input.email</td><td>input.phone</td> */}
+
+
+
+
+
+
+{/* this code did not work because I did not have the concatonated jquery command to search for the val entered by the user <td>$('input.first-name')</td><td>input.last-name</td><td>input.email</td><td>input.phone</td> */}
