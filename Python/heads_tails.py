27f87fa0402a9536d43f41411141665e8a1bfa69
Candra Davis <candrasd1029@gmail.com>
2016-11-30 13:11:24 -0600

    coin flips trips

--- Python/heads_tails.py
+++ Python/heads_tails.py
@@ -1,21 +1,25 @@
 # You're going to create a program that simulates tossing a coin 5,000 times. Your program should display how many times the head/tail appears.
 def create_random_numbers():
+    toss_list = []
     for count in range(0, 5):
         import random
         random_num = random.random()
-        toss_list = []
+        round(random_num)
         toss_list.append(random_num)
-    print toss_list
+    return toss_list
 
-    # return toss_list
-print create_random_numbers()
+heads_tails_list = create_random_numbers()
 
-# heads_tails_list = create_random_numbers()
-# def coin_toss():
-#     for num in heads_tails_list:
-#         num.round
-# print coin_toss()
-
+# print heads_tails_list
+def coin_toss():
+    count = 1
+    for num in heads_tails_list:
+        if num == 1:
+            print "Attempt #" + str(count) + ":  Throwing a coin...It's heads"
+        else:
+            print "Attempt #" + str(count) + ":  Throwing a coin...It's tails"
+        count +=1
+print coin_toss()
         # print "Attempt #" + str(i) + ":  Throwing a coin...It's "
 
 
