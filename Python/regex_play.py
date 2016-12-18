ff46022832548e6b117d57131ae78fdd0b074f17
Candra Davis <candrasd1029@gmail.com>
2016-12-18 12:42:48 -0600

    changes to regex.py file

--- Python/regex_play.py
+++ Python/regex_play.py
@@ -3,19 +3,25 @@ import re
 
 def get_matching_words(regex):
     words = ["aimlessness", "assassin", "baby", "beekeeper", "belladonna", "cannonball", "crybaby", "denver", "embraceable", "facetious", "flashbulb", "gaslight", "hobgoblin", "iconoclast", "issue", "kebab", "kilo", "laundered", "mattress", "millennia", "natural", "obsessive", "paranoia", "queen", "rabble", "reabsorb", "sacrilegious", "schoolroom", "tabby", "tabloid", "unbearable", "union", "videotape"]
-    for word in words:
-        match = re.search(r'/v')
-        if match:
-            print 'found', match.group()
-        # return [word for word in words if re.search(regex, word)]
-# All words that contain a “v”
 
+    return [word for word in words if re.search(regex, word)]
 
-# All words that contain a double-“s”
-# All words that end with an “e”
-# All words that contain an “b”, any character, then another “b”
-# All words that contain an “b”, at least one character, then another “b”
-# All words that contain an “b”, any number of characters (including zero), then another “b”
-# All words that include all five vowels in order
-# All words that only use the letters in “regular expression” (each letter can appear any number of times)
-# All words that contain a double letter
+# All words that contain a v
+print get_matching_words(r'([v])')
+
+# All words that contain a double-s
+print get_matching_words(r'([s]+[s])')
+# All words that end with an e
+print get_matching_words(r'([a-z]+)')
+# # All words that contain an b, any character, then another b
+get_matching_words(r'([b]+[a-z]+[b])')
+# # All words that contain an b, at least one character, then another b
+# get_matching_words()
+# # All words that contain an b, any number of characters (including zero), then another b
+# get_matching_words()
+# # All words that include all five vowels in order
+# get_matching_words()
+# # All words that only use the letters in (regular expression) (each letter can appear any number of times)
+# get_matching_words()
+# # All words that contain a double letter
+# get_matching_words()
