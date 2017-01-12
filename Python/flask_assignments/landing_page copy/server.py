670d4d94e3c100d8432248a8405a109be994dfd3
Candra Davis <candrasd1029@gmail.com>
2017-01-12 17:07:48 -0600

    restructured file tree

--- Python/flask_assignments/landing_page copy/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('ninjas.html')
-@app.route('/dojos/new')
-def dojo_form():
-    return render_template('dojos.html')
-app.run(debug=True)
